4c2f6f425bc643fb4055ec7347128e7fe14d8f02
stwobe <cwmbranmathstutor@gmail.com>
2017-05-27 18:29:36 +0100

    Set up initial repository

---
+++ career_goal_01.py
@@ -0,0 +1,6 @@
+# career_goal_01.py
+
+'''
+GOAL:
+	To work in Linux, Open Source and Python"
+'''
---
+++ to_do_01.py
@@ -0,0 +1,10 @@
+# to_do_01.py
+
+'''
+To Do:
+	Add this to Trello - month - goal - job stuff
+	Create a Github repo
+	Clone this local repo to Github
+	Clone in Tagadab
+	Turn into a Flask online CV
+'''
